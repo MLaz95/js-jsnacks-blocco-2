a3362b222ced1134ccdc4b6a142f26e037efd1a7
marco <lazzari.marco.ml@gmail.com>
2024-02-05 16:51:31 +0100

    jsnack4 script funziona

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -2,7 +2,21 @@
 Inserisci un numero, se è pari stampa il numero,
 se è dispari stampa il numero successivo. */
 
+let results = [];
+let userInput;
 
+while (userInput != 'end') {
+    userInput = prompt('Input a number or type "end" to stop playing.');
 
+    if(!isNaN(userInput)){
+        if (userInput % 2 == 0){
+        results.push(userInput);
+        alert(results);
+        } else {
+            results.push(Number(userInput) + 1);
+            alert(results);
+        }
+    } 
+}
 
-
+document.querySelector('#result').innerHTML = `Here are all your results <br> ${results}`
