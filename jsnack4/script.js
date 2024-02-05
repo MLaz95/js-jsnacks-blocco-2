b9af922aad0b10e9d00f8e1a413587d4db4f7828
marco <lazzari.marco.ml@gmail.com>
2024-02-05 16:59:11 +0100

    jsnack4 completato

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -3,12 +3,15 @@ Inserisci un numero, se è pari stampa il numero,
 se è dispari stampa il numero successivo. */
 
 let results = [];
+// user input is declared beforehand so we can keep track of it outside the loop
 let userInput;
 
+// loop ends when user decides to type end
 while (userInput != 'end') {
-    userInput = prompt('Input a number or type "end" to stop playing.');
+    userInput = prompt('Input an integer or type "end" to stop playing.');
 
-    if(!isNaN(userInput)){
+    // checks that input is a number and an integer
+    if(!isNaN(userInput) && Number.isInteger(Number(userInput))){
         if (userInput % 2 == 0){
         results.push(userInput);
         alert(results);
