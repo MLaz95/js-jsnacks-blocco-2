ba77f71200e110e1e245832cd9ac8970e2c6f3f8
marco <lazzari.marco.ml@gmail.com>
2024-02-05 15:49:53 +0100

    jsnack1 script base funziona

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,16 @@
 - Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
 - Continua a chiedere i numeri all’utente e a inserirli nell’arrayfino a quando
 la somma degli elementi è minore di 50 */
+
+// set up array and sum variable
+const array = [];
+let sum = 0;
+
+do {
+    const userInput = Number(prompt("Inserisci un numero"));
+    array.push(userInput);
+    sum += userInput;
+} while (sum < 50);
+
+document.querySelector('#result').innerHTML = `You input ${array}, the final value is ${sum}`
+
