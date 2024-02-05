0a53cc5add60c9b750d675584eecd6a8bda9739b
marco <lazzari.marco.ml@gmail.com>
2024-02-05 16:13:03 +0100

    jsnack2 script funziona

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -4,3 +4,21 @@
 oppure "il numero è troppo basso" nel momento in cui il numero
 detto dall'utente non è perfettamente uguale al numero estratto in modo casuale.
 - Quando l'utente trova il numero, fateglielo sapere! */
+
+// generates number between 1 and a 100
+const number = Math.floor(Math.random() * 100 + 1);
+console.log(number)
+
+do{
+    // asks the user a number and compares it to the random computer number
+    userGuess = Number(prompt('Guess what number I am thinking of'))
+    if (userGuess > number){
+        alert('Too high, guess again');
+    }else if (userGuess < number){
+        alert('Too low, guess again');
+    }
+
+    // stops when the user guesses right
+} while (userGuess != number);
+
+document.querySelector('#result').innerHTML = `You got it! ${userGuess} is correct`
